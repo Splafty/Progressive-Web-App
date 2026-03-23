32945e3177e549ce08fe2811710e3a7dfe0d8a1b
98LucjaRomanska <lucka090@gmail.com>
2026-03-23 14:27:08 +0100

    zmiany w app.js

--- app.js
+++ app.js
@@ -29,12 +29,27 @@ function getLocation() {
   );
 }
 
-function showMap()
-{
-
+function showMap() {
+  const map = L.map('map').setView([lat, lng], 13);
+ 
+  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
+ 
+  L.marker([lat, lng]).addTo(map)
+    .bindPopup('Miejsce zgłoszenia')
+    .openPopup();
 }
 
-function shareReport()
-{
-
+ 
+ 
+function shareReport() {
+  if (navigator.share) {
+    navigator.share({
+      title: 'Zgłoszenie miejskie',
+      text: `Problem w lokalizacji: ${lat}, ${lng}`,
+      url: location.href
+    });
+  } else {
+    alert('Web Share API nieobsługiwane');
+  }
 }
+
