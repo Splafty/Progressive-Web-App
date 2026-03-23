363281877829ab776bc566c1eeafdb5edfa2e2d4
Splafty <Splaft@wp.pl>
2026-03-23 14:22:14 +0100

    Location - funkcja i koordynaty

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+let lat, lng;
+
 async function initCamera()
 {
 
@@ -8,9 +10,23 @@ async function capturePhoto()
 
 }
 
-function getLocation()
-{
+function getLocation() {
+  navigator.geolocation.getCurrentPosition(
+    (position) => {
+      lat = position.coords.latitude;
+      lng = position.coords.longitude;
 
+      document.getElementById('lat').textContent = lat.toFixed(6);
+      document.getElementById('lng').textContent = lng.toFixed(6);
+      document.getElementById('gpsStatus').textContent = "Status: lokalizacja pobrana";
+
+      showMap();
+    },
+    (err) => {
+      document.getElementById('gpsStatus').textContent = "Błąd GPS";
+      console.error(err);
+    }
+  );
 }
 
 function showMap()
